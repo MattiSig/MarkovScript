705f9d24d86f39f31bca929f5a45981f77473cde
dannibje <daniel.sigur@gmail.com>
2016-10-13 22:34:17 +0000

    script stuff

--- server.js
+++ server.js
@@ -5,113 +5,137 @@ var cheerio = require('cheerio');
 var app     = express();
 var Markov = require("./markovChain.js");
 
+//define prefix for different units
+var CHARACTER_PREFIX = "\t\t\t\t";
+var LINE_PREFIX = "\n\n\t\t";
+
 app.get('/scrape', function(req, res){
 
-var url = 'http://www.imsdb.com/scripts/Clerks.html';
-var sentance;
+    var url = 'http://www.imsdb.com/scripts/Clerks.html';
+    var sentance;
+    var json;
 
-request(url, function(error, response, html){
-    if(!error){
-        var $ = cheerio.load(html);
+    request(url, function(error, response, html){
+        
+        if(!error){
+            var $ = cheerio.load(html);
 
-    var character, sceene, lines;
-    var json = { character : [], sceene : '', lines : ''};
+            var character, sceene, lines;
+            json = { character : [], sceene : '', lines : ''};
 
-    $('pre').filter(function(){
-        //var data = $(this);
-        $('b').each(function(index, a){
+            $('pre').filter(function(){
+                //var data = $(this);
+                $('b').each(function(index, a){
 
-            var string = $(a).text();
-            var first = string.slice(0,4);
-            var next = string.slice(4,5);
-            var name = string.slice(4,string.length-1);
+                    var string = $(a).text();
+                    var first = string.slice(0,4);
+                    var next = string.slice(4,5);
+                    var name = string.slice(4,string.length-1);
+                    
+                    if (!(next=="\t") && first == CHARACTER_PREFIX){
+                        json.character.push(name);
+                    }
+                });
+            });
             
-            if (!(next=="\t") && first == "\t\t\t\t"){
-                json.character.push(name);
-            }
-        });
-    });
-    
-    var data = ($("pre")
-    .clone()    //clone the element
-    .children() //select all the children
-    .remove()   //remove all the children
-    .end()  //again go back to selected element
-    .text());
-    
-    console.log(data.length);
-
-    for(var i=0; i < data.length-1; i++){
-        var temp = data.slice(i,i+4);
-        var next = data.slice(i+4,i+5);
-
-        if(temp == "\n\n\t\t" && !(next == "\t")){
-            var running= true;
-            var j = i+3;
+            var data = ($("pre")
+            .clone()    //clone the element
+            .children() //select all the children
+            .remove()   //remove all the children
+            .end()  //again go back to selected element
+            .text());
             
-            while(running){
-                var temp2 = data.slice(j, j+2)
-                if(temp2 == "\n\n"){
-                    var lines = data.slice(i+4,j);
-                    lines = lines.replace(/\n\t\t|\t/g,' ');
-                    lines = lines.replace(/\"/g,'');
-                    lines = lines.replace(/\\.\\.\\.|\\.\\.\\.\\./g," ... ");
-                    lines = lines.replace(/  |   /g," ");
+            console.log(data.length);
 
-                    json.lines = json.lines + lines + "# ";
-                    running = false;
+            for(var i=0; i < data.length-1; i++){
+                var temp = data.slice(i,i+4);
+                var next = data.slice(i+4,i+5);
+
+                if(temp == LINE_PREFIX && !(next == "\t")){
+                    var running= true;
+                    var j = i+3;
+                    
+                    while(running){
+                        var temp2 = data.slice(j, j+2)
+                        if(temp2 == "\n\n"){
+                            var lines = data.slice(i+4,j);
+                            lines = lines.replace(/\n\t\t|\t/g,' ');
+                            lines = lines.replace(/\"/g,'');
+                            lines = lines.replace(/\\.\\.\\.|\\.\\.\\.\\./g," ... ");
+                            lines = lines.replace(/  |   /g," ");
+
+                            json.lines = json.lines + lines + "# ";
+                            running = false;
+                        }
+
+                        j++
+                        if(j >125040){
+                            running = false;
+                        }
+                    }
                 }
+            }
 
-                j++
-                if(j >125040){
-                    running = false;
+            for(var i=0; i<data.length-1; i++){
+                var temp = data.slice(i,i+3);
+                var next = data.slice(i+3,i+4);
+
+                if(temp == "\n\n\t" && !(next == "\t")){
+                    var running= true;
+                    var j = i+3;
+                    
+                    while(running){
+                        var temp2 = data.slice(j, j+2)
+                        if(temp2 == "\n\n"){
+                            var sceene = data.slice(i+3,j);
+                            var fixedSceene = sceene.replace(/\n\t/g,' ');
+                            json.sceene = json.sceene + fixedSceene + "#";
+                            running = false;
+                        }
+
+                        j++
+                        if(j >125040){
+                            running = false;
+                        }
+                    }
                 }
             }
         }
-    }
 
-    for(var i=0; i<data.length-1; i++){
-        var temp = data.slice(i,i+3);
-        var next = data.slice(i+3,i+4);
+        script = "";
 
-        if(temp == "\n\n\t" && !(next == "\t")){
-            var running= true;
-            var j = i+3;
-            
-            while(running){
-                var temp2 = data.slice(j, j+2)
-                if(temp2 == "\n\n"){
-                    var sceene = data.slice(i+3,j);
-                    var fixedSceene = sceene.replace(/\n\t/g,' ');
-                    json.sceene = json.sceene + fixedSceene + "#";
-                    running = false;
-                }
+        //create scene
 
-                j++
-                if(j >125040){
-                    running = false;
-                }
-            }
+        //create convo
+        characters = json.character;
+        for(var i = 0 ; i<10 ; i++){
+            var character = characters[Math.floor(Math.random()*characters.length)];
+            var line = Markov(json.lines);
+
+            //add character says
+            script += '\n\n' + CHARACTER_PREFIX + character;
+
+            //add line that character says
+            script += '\n\t\t' + line;
         }
-    }
 
-    sentance = Markov(json.lines);
-}
+        fs.writeFile('script.txt', script,function(err){
+            console.log('noice');
+            res.sendFile('script.txt', {root: __dirname});
+        })
+        // To write to the system we will use the built in 'fs' library.
+        // In this example we will pass 3 parameters to the writeFile function
+        // Parameter 1 :  output.json - this is what the created filename will be called
+        // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
+        // Parameter 3 :  callback function - a callback function to let us know the status of our function
 
-// To write to the system we will use the built in 'fs' library.
-// In this example we will pass 3 parameters to the writeFile function
-// Parameter 1 :  output.json - this is what the created filename will be called
-// Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
-// Parameter 3 :  callback function - a callback function to let us know the status of our function
+        // fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
 
-fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+        //     console.log('File successfully written! - Check your project directory for the output.json file');
 
-    console.log('File successfully written! - Check your project directory for the output.json file');
+        // });
 
-});
-
-// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
-res.send(sentance);
+        // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
 
     });
 });
